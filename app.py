92ef20eac7257eea39a52967ed661c4cac914639
donahula <donahula@oregonstate.edu>
2021-07-11 17:26:05 -0700

    Added web application files

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    count = "1"
+	count = "1"
 	return render_template("index.html", count=count)
 	
 if __name__ == "__main__":
-    app.run()
+	app.run()
