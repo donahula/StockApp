6cfb2474195e0744d1b46b4027a2457a740c6243
donahula <donahula@oregonstate.edu>
2021-07-11 18:20:51 -0700

    a

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from yahoo_finance import Share
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	count = "1"
-	return render_template("index.html", count=count)
+	f = open('nasdaq_100_stock_list.json',)
+	stock_list = json.load(f)
+	
+	return render_template("index.html", count=count, stocks=stock_list)
 	
 if __name__ == "__main__":
 	app.run()
