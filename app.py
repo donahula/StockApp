bffdc670a38990f57da41198e2d68139fb04de93
donahula <donahula@oregonstate.edu>
2021-07-13 21:42:57 -0700

    asdf

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from yahoo_finance import Share
 import json
+from flask import request
+import yfinance as yf
+from alpha_vantage.timeseries import TimeSeries
 
+ALPHA_VANTAGE_API_KEY = 'TTZ38CINRJYC1KGO' 
 
 app = Flask(__name__)
 
@@ -11,6 +15,23 @@ def index():
 	stock_list = json.load(f)
 
 	return render_template("index.html", stocks=stock_list)
+
+@app.route("/data_query", methods=["POST"])
+def data_query():
+	req = request.get_json()
 	
+	ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='json')
+	print(help(ts))
+	#testing = ts.get_daily_adjusted('AAPL', outputsize='compact')
+	print(testing)
+	
+	#for i in req:
+#		print(i)
+		#print(yf.download(i,'2018-01-01','2018-01-04'))
+	
+	
+	
+	return "test"
+
 if __name__ == "__main__":
 	app.run()
